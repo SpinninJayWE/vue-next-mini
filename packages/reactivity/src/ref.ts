2e2ba43600db11b311665ed4fe4b552d13a267d0
SpinninJayWE <1315025826@qq.com>
2023-02-14 17:11:49 +0800

    完善ref功能 通过set value 方法 手动依赖触发

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,6 @@
+import { hasChanged } from '@vue/shared'
 import { Dep, createDep } from './dep'
-import { activeEffect, trackEffects } from './effect'
+import { activeEffect, trackEffects, triggerrEffects } from './effect'
 import { toReactive } from './reactive'
 
 export interface Ref<T = any> {
@@ -20,11 +21,13 @@ function createRef(rawValue: unknown, shallow: boolean) {
 
 class RefImpl<T> {
 	private _value: T
+	private _rawValue: T
 
 	public dep?: Dep = undefined
 
 	public readonly __v__isRef = true
 	constructor(value: T, public readonly __v__isShallow: boolean) {
+		this._rawValue = value
 		this._value = __v__isShallow ? value : toReactive(value)
 	}
 
@@ -33,12 +36,35 @@ class RefImpl<T> {
 		return this._value
 	}
 
-	set value(newValue) {}
+	set value(newValue) {
+		if (hasChanged(newValue, this._rawValue)) {
+			this._rawValue = newValue
+			this._value = toReactive(newValue)
+			triggerRefValue(this)
+		}
+	}
 }
 
+/**
+ *
+ * @param ref
+ * 收集依赖
+ */
 export function trackRefValue(ref) {
 	if (activeEffect) {
 		trackEffects(ref.dep || (ref.dep = createDep()))
+	}
+}
+
+/**
+ * 触发依赖
+ * @param
+ * @returns
+ */
+
+export function triggerRefValue(ref) {
+	if (ref.dep) {
+		triggerrEffects(ref.dep)
 	}
 }
 
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,3 +6,12 @@ export const isArray = Array.isArray
 
 export const isObject = (val: unknown) =>
 	val !== null && typeof val === 'object'
+
+/**
+ *
+ * @param
+ * @param 对比两个数据是否发生改变
+ * @returns
+ */
+export const hasChanged = (value: any, oldVal: any): boolean =>
+	!Object.is(value, oldVal)
